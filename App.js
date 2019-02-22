f5c519bf389d07c35319291108abef13b700ff7f
Patty Case <beadypatty@yahoo.com>
2019-02-22 00:30:48 -0800

    Added discount functionality. Added total calculation.

--- App.js
+++ App.js
@@ -3,12 +3,24 @@
  */
 
 import React, {Component} from 'react';
-import {AppRegistry, Platform, StyleSheet, Text, View, Button, Image, Linking, Modal, LayoutAnimation, UIManager, TouchableOpacity} from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import {AppRegistry, Platform, StyleSheet, Text, TextInput, FlatList, View, Button, Image, Linking, Modal, LayoutAnimation, UIManager, TouchableOpacity, TouchableHighlight} from 'react-native';
+import ListItem from './components/ListItem';
+import { connect } from 'react-redux';
+import { addPlace } from './actions/place';
 import data from './price.json';
 
 const pickupText = 'Picking up your order in the store helps cut costs, and we pass the savings on to you.'
-
-export default class PurchaseSummary extends Component {
+	 
+class PurchaseSummary extends Component {
+	
+	getTotal = () => {
+		var price = parseFloat(data.items[0].price, 10);
+		var savings = parseFloat(data.items[0].savings, 10);
+		var tax = parseFloat(data.items[0].taxes, 10);
+		return price - savings + tax;
+	};  
 	
     constructor() {
         super();
@@ -20,12 +32,51 @@ export default class PurchaseSummary extends Component {
         };
  
         this.state = { onLayoutHeight: 0, modifiedHeight: 0, expanded: false }
- 
+ 	   	this.state.modalVisible = false
+ 	   	this.state.estimatedTotal = this.getTotal().toString()
+		
         if( Platform.OS === 'android' ) {
           UIManager.setLayoutAnimationEnabledExperimental( true )
         }
     }
- 
+	
+	state = {
+	 	code: '',
+	   	estimatedTotal: '',
+	   	places: [],
+		modalVisible: false
+	}
+
+	codeSubmitHandler = () => {
+	   if (this.state.code.trim() === '') {
+	      return;
+	   }
+	   if (this.state.code === 'DISCOUNT') {
+		   this.state.  estimatedTotal = this.getDiscount().toString();
+	   	   this.props.add(this.state.  estimatedTotal);
+   	   }
+	}
+	
+	estimatedTotalChangeHandler = (value) => {
+		 this.setState({
+	 	   code: value
+	 	 });    
+	}
+
+	placesOutput = () => {
+		  return (
+		   <FlatList style = { styles.listContainer }
+		     data = { this.props.places }
+		     keyExtractor={(item, index) => index.toString()}
+		     renderItem = { info => (
+		       <ListItem 
+		           estimatedTotal={ info.item.value }
+		       />
+		     )}
+		  />
+		 )
+	}
+		
     changeLayout = () => {
         LayoutAnimation.configureNext( LayoutAnimation.Presets.easeInEaseOut );
  
@@ -38,10 +89,6 @@ export default class PurchaseSummary extends Component {
     getViewHeight( height ) {
         this.setState({ onLayoutHeight: height });
     }
-	
-    state = {
-      modalVisible: false,
-    };
   
     _handleButtonPress = () => {
       this.setModalVisible(true);
@@ -50,6 +97,12 @@ export default class PurchaseSummary extends Component {
     setModalVisible = (visible) => {
       this.setState({modalVisible: visible});
     };
+	
+	getDiscount = () => {
+		var total = this.getTotal();
+		var discountPrice = total - total/10;
+		return discountPrice.toFixed(2);
+	}
 	
   render() {
       var modalBackgroundStyle = {
@@ -66,9 +119,9 @@ export default class PurchaseSummary extends Component {
 	  
     return ( 
 		<View style = {styles.container}>
-		
+		 	
     		<Modal
-        	  animationType='fade'
+        	  animationType='none'
         	  transparent={true}
         	  visible={this.state.modalVisible}
         	  onRequestClose={() => this.setModalVisible(false)}
@@ -91,17 +144,17 @@ export default class PurchaseSummary extends Component {
       		</Text>
 
 			<Text style={{alignSelf: 'flex-end'}}>
-			  {data.items[0].subtotal}{"\n"}{"\n"}
+			  {data.items[0].price}{"\n"}{"\n"}
 			</Text>  
       
 			<Text style={{textDecorationLine: 'underline'}} onPress={this._handleButtonPress}>
          	  Pickup savings
       		</Text>
-
+			  
 			<Text style={{alignSelf: 'flex-end', color: 'red'}}>
 			  -{data.items[0].savings}{"\n"}{"\n"}
 			</Text>    
-      
+      		  
       		<Text>
          	  Est taxes & fees{"\n"}
 	   	  	  (Based on 95070)
@@ -115,12 +168,11 @@ export default class PurchaseSummary extends Component {
 	   	 	  {"\n"}
          	  Est total
       	  	</Text>
-			  
-			    
+			  	    
 			<Text style={{alignSelf: 'flex-end'}}>
-			  {data.total}{"\n"}{"\n"}
+			  {this.state.estimatedTotal}{"\n"}{"\n"}
 			</Text>     
-		  
+		  		  
            <View style = { styles.btnTextHolder }>
                <TouchableOpacity activeOpacity = { 0.0 } onPress = { this.changeLayout } style = { styles.Btn }>
 			    <View style = {{flexDirection:'row', flexWrap:'wrap'}}>
@@ -132,7 +184,7 @@ export default class PurchaseSummary extends Component {
 			    </View>
                </TouchableOpacity>
                <View style = {{height: this.state.modifiedHeight, overflow: 'hidden'}}>
-				   <View style = {{flexDirection:'row', flexWrap:'wrap'}}>
+				   <View style = { styles.inputContainer }>
           	   	   	 <Image
               		 	style={styles.buttonImage}
               		 	source={itemImage}>
@@ -143,7 +195,7 @@ export default class PurchaseSummary extends Component {
 				  </View>
                </View>
             </View>
-			  
+			  					  	  
            <View style = { styles.btnTextHolder }>
                <TouchableOpacity activeOpacity = { 0.0 } onPress = { this.changeLayout } style = { styles.Btn }>
 				 <View style = {{flexDirection:'row', flexWrap:'wrap'}}>
@@ -155,18 +207,49 @@ export default class PurchaseSummary extends Component {
 				 </View>
                </TouchableOpacity>
                <View style = {{ height: this.state.modifiedHeight, overflow: 'hidden' }}>
-                   <Text style = { styles.text } onLayout = {( event ) => this.getViewHeight( event.nativeEvent.layout.height )}>
-                       Promo code
-                   </Text>
+  		 	      <View style = { styles.inputContainer }>
+  		 	        <TextInput
+  		 	          placeholder = "Promo code"
+  		 	          style = { styles.placeInput }
+  		 	          value = { this.state.code }
+  		 	          onChangeText = { this.estimatedTotalChangeHandler }
+  		 	        ></TextInput>
+  		 	        <Button title = 'Apply' 
+  		 	          style = { styles.placeButton }
+  		 	          onPress = { this.codeSubmitHandler }
+  		 	        />
+  		 	        </View>
+  		 	        <View style = { styles.listContainer }>
+  		 	          { this.placesOutput() }
+  		 	        </View>		
                </View>
             </View>
-   		
 		</View>
     );
   }
 }
 
 const styles = StyleSheet.create ({
+    container2: {
+      paddingTop: 30,
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+    },
+    inputContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      alignItems: 'center',
+      width: '100%'
+    },
+    placeInput: {
+      width: '70%'
+    },
+    placeButton: {
+      width: '30%'
+    },
+    listContainer: {
+      width: '100%'
+    },
    container: {
 	  flex: 1,
       marginTop: 100,
@@ -213,12 +296,10 @@ const styles = StyleSheet.create ({
 	   textDecorationLine: 'underline'
 	   
    },
-
    btnTextHolder: {
        borderWidth: 1,
        borderColor: '#F5FCFF'
    },
-
    Btn: {
        padding: 10,
        backgroundColor: '#F5FCFF'
@@ -228,3 +309,19 @@ const styles = StyleSheet.create ({
        height  : 25
    },
 });
+
+const mapStateToProps = state => {
+  return {
+    places: state.places.places
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    add: (name) => {
+      dispatch(addPlace(name))
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(PurchaseSummary)
